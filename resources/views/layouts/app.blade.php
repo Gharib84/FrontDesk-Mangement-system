f0078a73dc6e54f03ff9bed4d833c875aa0a0597
gharib84 <itprofessional63@gmail.com>
2022-11-14 15:01:50 +0100

    logo has finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Front Desk Management') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -31,5 +31,6 @@
                 {{ $slot }}
             </main>
         </div>
+        
     </body>
 </html>
