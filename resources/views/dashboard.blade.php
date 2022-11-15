f26c53ce6898c1c7c9d48955529ab56f072a03f6
gharib84 <itprofessional63@gmail.com>
2022-11-15 17:09:46 +0100

    front desk system text created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('F.D.System') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200 text-center">
-                    Welcome To Paradise Resort System
+                    
                     {{-- cards --}}
                     <div
                         class="cards mt-7 w-full h-2/4  sm:flex sm:flex-col md:flex md:flex-col lg:flex lg:flex-row  xl:flex xl:flex-row 2xl:flex 2xl:flex-row justify-between
@@ -17,10 +17,10 @@
                         ">
                         <div class="card-one card w-full h-48 flex justify-center items-center shadow-md ml-5 rounded-r">
                             <form action="" method="POST">
-                                <h2>
+                                <h1 class="text-3xl">
                                     Make A Book
-                                </h2>
-                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-1" style="color: aliceblue;font-weight:bold;">
+                                </h1>
+                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-2" style="color: aliceblue;font-weight:bold;">
                                     Create
                                 </button>
                             </form>
@@ -28,10 +28,10 @@
 
                         <div class="card-two card w-full h-48 flex justify-center items-center shadow-md ml-5 rounded-r">
                             <form action="" method="POST">
-                                <h2>
+                                <h1 class="text-3xl">
                                    Check Arrivals
-                                </h2>
-                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-1" style="color: aliceblue; font-weight:bold;">
+                                </h1>
+                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-2" style="color: aliceblue; font-weight:bold;">
                                     Search
                                 </button>
                             </form>
@@ -39,10 +39,10 @@
 
                         <div class="card-three card w-full h-48 flex justify-center items-center shadow-md ml-5 rounded-r">
                             <form action="" method="POST">
-                                <h2>
+                                <h1 class="text-3xl">
                                     Departure
-                                </h2>
-                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-1" style="color: aliceblue;font-weight:bold;">
+                                </h1>
+                                <button class="bg-indigo-800 px-5 py-2 rounded-r mt-2" style="color: aliceblue;font-weight:bold;">
                                     Check
                                 </button>
                             </form>
